6bc3fa81269bcf2359be55347507c2880967b4da
Cody C <codyicastro@gmail.com>
2020-06-29 16:40:02 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,8 @@
 def find_max_value(array)
   count = 0
   while count < array.length do
-    if array[count] == 5
-      return count
+    if array[count] == array.max
+      return array[count]
     end
     count += 1
   end
